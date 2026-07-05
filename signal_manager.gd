e830040db8b73086a3456c1ee0fd9eaf3212dd6e
Topeni <matysekz@centrum.cz>
2026-07-05 17:09:55 +0200

    added a base signal manager

---
+++ signal_manager.gd
@@ -0,0 +1,9 @@
+extends Node
+
+@warning_ignore("unused_signal")
+signal clock_stop
+@warning_ignore("unused_signal")
+signal clock_start
+signal clock_add_time(value : float)
+@warning_ignore("unused_signal")
+signal send_message(message : String)
